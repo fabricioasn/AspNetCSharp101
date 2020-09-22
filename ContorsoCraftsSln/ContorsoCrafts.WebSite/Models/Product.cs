b63a7aefa537714f7c1d84c98b4cf4ce7cdc3f1a
Fabricio Almeida da Silva Nunes <32113848+fabricioasn@users.noreply.github.com>
2020-09-22 03:06:17 -0300

    ContorsoCrafts razor page ASP.NET addition

---
+++ ContorsoCraftsSln/ContorsoCrafts.WebSite/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace ContorsoCrafts.WebSite.Models
+{
+    public class Product
+    {
+        [JsonPropertyName("Id")]
+        public string Id { get; set; }
+        public string Maker { get; set; }
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+   
+
+    //overriding tostrigng to serialize JSON into object
+    public override string ToString() =>    
+        //serialize product strings back to JSON
+        JsonSerializer.Serialize<Product>(this);
+
+    }
+}
